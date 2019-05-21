91365bec9e8e417abea7257fb9740a835fe3c16c
ChristofferNorgaard <50552689+ChristofferNorgaard@users.noreply.github.com>
2019-05-21 11:59:26 +0200

    Update math_analitics.py

--- analitika/math_analitics.py
+++ analitika/math_analitics.py
@@ -2,12 +2,13 @@
 def average(GyroObject_arr):
     y = []
     x = []
-    for i in range(0, len(GyroObject_arr[0].y_ar)):
+    minlen = min([len(y.y_ar) for x in GyroObject_arr[0]])
+    for i in range(0, minlen)):
         sum_list_x = [x.x_ar[i] for x in GyroObject_arr]
         sum_list_y = [x.y_ar[i] for x in GyroObject_arr]
         x.append(sum(sum_list_x)/len(GyroObject_arr))
         y.append(sum(sum_list_y)/len(GyroObject_arr))
 
-    return y
+    return y, x
         
         

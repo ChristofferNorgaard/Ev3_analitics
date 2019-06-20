5a31fdd2066b3467991d7077e6be902efb4c53c2
ChristofferNorgaard <50552689+ChristofferNorgaard@users.noreply.github.com>
2019-06-20 18:06:26 +0200

    Delete setup.py

--- analitika/setup.py
+++
@@ -1,8 +0,0 @@
-import sql_agent as sql
-import os
-#create a data_base
-sql_ag = sql.sql_data('test.db','test')
-try:
-    sql_ag.create_db()
-except:
-    print('The database is already created.')
